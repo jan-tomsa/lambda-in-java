499b19578edad1d6243562f5f4621b331aa67b41
jan-tomsa <>
2011-10-16 22:02:48 +0200

    Enhanced App to actually do something - serve as a showcase.

--- src/main/java/com/tomsovi/App.java
+++ src/main/java/com/tomsovi/App.java
@@ -1,23 +1,42 @@
 package com.tomsovi;
 
+import java.util.List;
+
 /**
- * Hello world!
+ * Excercise some tricks with SelectableList
  *
  */
 public class App 
 {
+    SelectableList<Integer> list = new SelectableList<Integer>();
+
+    private void execute(final Integer limit) {
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(100);
+        list.add(200);
+        list.add(1000);
+        List<Integer> newList = list.select(new Selector<Integer>() {
+                                            public boolean evaluate(Integer n) {
+                                                return n > limit;
+                                            }
+                                        });
+        System.out.println( "The list has " + list.size() + " items." );
+        System.out.println( "The sublist (> "+limit+") has " + newList.size() + " items." );
+    }
+    
     public static void main( String[] args )
     {
-        SelectableList sl = new SelectableList();
-        sl.add(1);
-        sl.add(2);
-        sl.add(3);
-        sl.add(10);
-        sl.add(20);
-        sl.add(30);
-        sl.add(100);
-        sl.add(200);
-        sl.add(1000);
-        System.out.println( "The list has " + sl.size() + " items." );
+        App app = new App();
+        System.out.println("args.length: " + args.length);
+        if (args.length>0) {
+            app.execute(Integer.parseInt(args[0]));
+        } else {
+            app.execute(10);
+        }
     }
 }
